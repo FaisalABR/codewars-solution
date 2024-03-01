1dfdcf4df0ccd0f368da1184d16481f00b1c5049
FaisalABR <faisalabubakar92@gmail.com>
2024-03-01 15:19:09 +0700

    learning: bubble sort

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([5, 1, 2, 8, 3, 4, 7]));
+
+// youtube https://www.youtube.com/watch?v=nmhjrI-aW5o
