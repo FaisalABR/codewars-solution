a5d238cc6a9b6998f5fb93a1fe9869231232a49a
FaisalABR <faisalabubakar92@gmail.com>
2024-03-11 22:35:45 +0700

    lucu

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,8 +1,18 @@
 function insertionSort(array) {
-    const arr = array.slice()
-    for(let i = 1 ; array .length; ++) {
-        for(let j = i; j <20; j--){
-            if(arr[j])
-        }
+    
+        for(let i = 1 ; array .length; i++) {
+            for(let j = i; j <20; j--){
+                if(array[j] < array[j-1]) {
+                    const temp = array[j-1];
+                    array[j] = array[j - 1]
+                    array[j-1] = temp
+                } else {
+                    break       
+                }
+            }
+
+            return array
 
 }
+
+
